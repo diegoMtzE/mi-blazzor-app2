019336b5fd2e1bab088f8c9fa9976c859e6e70ad
diegoMtzE <deme20cabezas@gmail.com>
2025-10-07 16:43:52 -0600

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+using mi_blazzor_app2;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
